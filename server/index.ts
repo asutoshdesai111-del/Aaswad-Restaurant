0da2d902ea8fa9d58ece1b0b8e078dc00f1e6700
Ashu <asutoshdesai111@gmail.com>
2026-01-11 20:02:22 +0530

    Initial Commit

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,4 @@
+import "dotenv/config"; 
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { serveStatic } from "./static";
@@ -61,6 +62,8 @@ app.use((req, res, next) => {
 
 (async () => {
   await registerRoutes(httpServer, app);
+
+  console.log("db url", process.env.DATABASE_URL);
 
   app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
     const status = err.status || err.statusCode || 500;
